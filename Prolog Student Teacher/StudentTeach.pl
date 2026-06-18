e1949162c66b1d9fa7c2252063b97650c7ba74f8
192525379simats-Prakash-balaji <192525379.simats@saveetha.com>
2026-06-18 14:20:47 +0530

    Add files via upload

---
+++ Prolog Student Teacher/StudentTeach.pl
@@ -0,0 +1,4 @@
+student_teacher_subject(prakash, dr_smith, cs101).
+student_teacher_subject(rahul,   dr_smith, ai201).
+student_teacher_subject(anita,   dr_jones, cs101).
+student_teacher_subject(kiran,   dr_brown, ml301).
